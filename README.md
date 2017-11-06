c59cb7f03ed018b0664ad492174b87103b030177
Patrice FERLET <metal3d@gmail.com>
2017-11-06 11:26:36 +0100

    Add source and doc

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# redis-ellison
-Ellison, Master of sentinel for client that cannot use them
+# Redis-Ellison
+
+Ellison, Master of sentinels for client that cannot use them.
+
+Redis provides sentinels to elect a leader (master) that is able to write data when a master falls. That is very impressive and allows Redis High Availability. But sometimes, clients (frameworks, libraries...) are not able to use sentinels. Actually, the client must know if sentinels exists and should make a request to get master address to write data.
+
+Redis-Ellison is a proxy that will interact with sentinels and master to send and get data from the master (as this time, it cannot make distinction between get and set), so that clients can connect Redis Ellison as if it was the master, and ellison will make data routing.
+
+![](docs/diagram.png)
+
+Ellison is scallable, so you can use it in a PaaS as Openshift/Kubernetes and scale up. The "service" will load-balance on each Redis-Ellison server.
+
+
+NOTE: At this time, you need "redis-cli" command installed on your server to be able to contact sentinels. That should change in later revisions.
+
+
+# Installation
+
+At this time, you should use "`go get`" command, but there will be releases with binaries and Docker images very soon.
+
+```
+go get -u github.com/metal3d/redis-ellison
+```
+
+# Usage
+
+```
+Usage of ellison:
+  -cluster string
+    	redis cluster name (default "mymaster")
+  -redis-command string
+    	redis command to connect to sentinel, it can also be "docker exec sentinel1 redis-cli" for example (default "redis-cli")
+  -sentinel-host string
+    	sentinel host or ip (default "127.0.0.1")
+  -sentinel-port string
+    	sentinel port to connect (default "26379")
+```
+
+Note: each flag can be overriden by an environment variable that respect the flag.:
+
+- `CLUSTER`
+- `REDIS_COMMAND`
+- `SENTINEL_HOST`
+- `SENTINEL_PORT`
+
+So that, it's simpler to inject configuration in a Docker container.
+
+# In OpenShift/Kubernetes
+
+Sentinel host/port should be set to the service managing sentinels. So that if a sentinel is lost, the route will be set to another sentinel.
+
+
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	mux          = &sync.Mutex{}
+	clusterName  = "mymaster"
+	redisCommand = "redis-cli"
+	sentinelHost = "127.0.0.1"
+	sentinelPort = "26379"
+	redisMaster  = ""
+	oldMaster    = ""
+	maxTries     = 10
+	triesDelay   = time.Duration(1 * time.Second)
+)
+
+func getMasterAddr() {
+	command := strings.Split(redisCommand, " ")
+	command = append(command,
+		"-h", sentinelHost,
+		"-p", sentinelPort,
+		"SENTINEL", "get-master-addr-by-name", clusterName)
+
+	cmd := exec.Command(command[0], command[1:]...)
+
+	var out bytes.Buffer
+	var er bytes.Buffer
+
+	cmd.Stdout = &out
+	cmd.Stderr = &er
+
+	if err := cmd.Start(); err != nil {
+		log.Println(err)
+		log.Println("err::", er.String())
+		return
+	}
+	cmd.Wait()
+
+	m := strings.Split(out.String(), "\n")
+
+	mux.Lock()
+	redisMaster = fmt.Sprintf("%s:%s", m[0], m[1])
+	if redisMaster != oldMaster {
+		log.Println("redis master addr", redisMaster)
+		oldMaster = redisMaster
+	}
+	mux.Unlock()
+}
+
+func handleConn(client net.Conn) {
+	var masterConn net.Conn
+	var err error
+
+	// first attempt
+	mux.Lock()
+	masterAddr := redisMaster
+	mux.Unlock()
+	masterConn, err = net.DialTimeout("tcp", masterAddr, time.Duration(1*time.Second))
+
+	// note that this loop will not happend if err is nil
+	for i := 0; i < maxTries && err != nil; i++ {
+		time.Sleep(triesDelay)
+		getMasterAddr()
+		mux.Lock()
+		masterAddr = redisMaster
+		mux.Unlock()
+		masterConn, err = net.DialTimeout("tcp", masterAddr, time.Duration(1*time.Second))
+	}
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// proxy !
+	go dataCopy(masterConn, client)
+	go dataCopy(client, masterConn)
+}
+
+func dataCopy(from, to net.Conn) {
+	defer from.Close()
+	io.Copy(from, to)
+	log.Println("Connection is now close", from.RemoteAddr())
+}
+
+func main() {
+	flag.StringVar(&clusterName, "cluster", clusterName,
+		"redis cluster name")
+	flag.StringVar(&redisCommand, "redis-command", redisCommand,
+		"redis command to connect to sentinel, it can also be \"docker exec sentinel1 redis-cli\" for example")
+	flag.StringVar(&sentinelHost, "sentinel-host", sentinelHost,
+		"sentinel host or ip")
+	flag.StringVar(&sentinelPort, "sentinel-port", sentinelPort,
+		"sentinel port to connect")
+	flag.Parse()
+	overFlag()
+
+	// be sure that master address is
+	// (yes, it's not perfect, but it can save some slave/master confusion problems when redis cluster is completly restarted)
+	go func() {
+		for {
+			select {
+			case <-time.Tick(1 * time.Second):
+				getMasterAddr()
+			}
+		}
+	}()
+
+	// now, wait for clients and handle connections to the master
+	c, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := c.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+// override flag name by their environment variable if found.
+func overFlag() {
+	flag.VisitAll(func(f *flag.Flag) {
+		env := strings.ToUpper(f.Name)
+		env = strings.Replace(env, "-", "_", -1)
+		if v := os.Getenv(env); v != "" {
+			flag.Set(f.Name, v)
+		}
+	})
+}
